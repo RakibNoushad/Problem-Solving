940a7384c6e045ef66e4a693f4807583cfcd73b0
RakibNoushad <rakibnoushad@gmail.com>
2020-05-10 20:02:54 +0600

    URI solve added

---
+++ URI/1145 - Logical Sequence 2.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long int  ULLI;
+typedef long long int  LLI;
+typedef vector<int> vi;
+
+#define SQR(x) ((x)*(x))
+#define pb push_back
+#define pw(x,y) (int)pow(x,y)+0.5
+#define ub(a,b)  upper_bound(a.begin(),a.end(),b)-a.begin()
+#define lb(a,b)  lower_bound(a.begin(),a.end(),b)-a.begin()
+#define srt(a)   sort(a.begin(),a.end())
+#define wl(t)    cin >> t; while(t--)
+#define fr(t)  cin >> t; for(i=0;i<t;i++)
+
+const int MX = 1e5 + 7 ;
+const int INF = 1e8 + 7 ;
+ 
+char x[200010],y[200010],z[200010];
+LLI a[200010],b[200010],c[200010],d[200010];
+
+ 
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+    LLI n=1, t, i, j,k,mn=200,m,pos,mx=0,l,dif,f=1;
+    LLI sum=0;
+
+    cin >> t >> n;
+    for(i=1;i<=n;i++)
+    {
+      sum++;
+      cout << i;
+      if(sum%t && i!=n) cout << " ";
+      else cout << endl;
+    }
+
+   
+
+return 0;
+}
